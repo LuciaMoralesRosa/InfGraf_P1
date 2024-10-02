c92e876631c92ce33a9162d3e481f738ead152c1
Claudia <kittykatnoon@gmail.com>
2024-10-02 16:24:19 +0200

    test update

--- tests/intersecciontest.cpp
+++ tests/intersecciontest.cpp
@@ -8,19 +8,22 @@ using namespace std;
 // g++ main.cpp rayo.cpp esfera.cpp -o interseccion
 
 int main() {
-    Rayo rayo 
-    Vector3 P0 = {0, 0, 0};  // Origen del rayo
-    Vector3 d = {1, 1, 1};   // Dirección del rayo (normalizada)
-    Vector3 C = {5, 5, 5};   // Centro de la esfera
+    Punto3D P0(0,0,0);  // Origen del rayo
+    Direccion d(1,1,1);   // Dirección del rayo (normalizada)
+    Rayo rayo(Punto3D P0, Direccion d);
+    Punto3D C(5,5,5);   // Centro de la esfera
     double r = 2.0;          // Radio de la esfera
+    Esfera esfera(C, r);
 
-    auto [intersecta, t1, t2] = intersectRaySphere(P0, d, C, r);
+    vector<Punto3D> intersecciones = esfera.interseccionRayo(rayo);
 
-    if (intersecta) {
+    cout << intersecciones;
+
+    /*if (intersecta) {
         std::cout << "El rayo intersecta la esfera en t1 = " << t1 << " y t2 = " << t2 << std::endl;
     } else {
         std::cout << "El rayo no intersecta la esfera." << std::endl;
-    }
+    }*/
 
     return 0;
 }
